8123a63343d0de8548b18ccade8df987669ec305
Diego Aredes <104737872+AredesDiego@users.noreply.github.com>
2022-05-29 09:34:47 -0300

    Add files via upload

--- Simple Calculator.cpp
+++ Simple Calculator.cpp
@@ -5,72 +5,104 @@ int sum (int , int );
 int subtract (int ,int );
 int multiply (int , int );
 int division (int , int );
-int resta (int , int );
 
 int main (){
-
+// name of the calculator
 system ("Title Calculator");
-int a,b,x;
-string f;
+//declare variables
 
+int num1,num2,choise;
+string answer;
+
+//declare your choise
 do {
 system ("cls");
 cout << "CHOISE YOUR DESTINY :"<< endl<< "1-SUM"<<endl<<"2-SUBTRACT"<<endl<<"3-MULTIPLY"<<endl<<"4-DIVISION"<<endl<<endl;
 cout << "THE NUMBER OF YOUR CHOISE : " ;
-cin >> x;
-cout << "PUT YOUR FIRST NUMBER : ";
+cin >> choise;
 
-switch(x){
+// your choise
+switch(choise){
 case 1:
+	do{
 	system ("Title SUM");
-	cin >>a;
+	system ("cls");
+	cout << "PUT YOUR FIRST NUMBER : " ;
+	cin >>num1;
 	cout << "YOUR SECOND NUMBER : ";
-	cin >> b;cout <<endl;
-	cout << "THE ANSWER IS : "<< sum (a,b)<<endl<<endl;
+	cin >> num2;
+	cout << endl<<"THE ANSWER IS : "<< sum (num1,num2)<<endl<<endl; 
+	//this is to repeat
+	cout << "DO YOU WANNA CONTINUE WITH THE SUM? IF THAT IS RIGHT PRESS Y"<<endl;
+	cin >> answer;
+	}while (answer=="Y"|| answer =="y");
 	break;
 	
 case 2:
+	do{
 	system ("Title SUBTRACT");
-	cin >>a;
+	system ("cls");
+	cout << "PUT YOUR FIRST NUMBER : " ;
+	cin >>num1;
 	cout << "YOUR SECOND NUMBER : ";
-	cin >> b;cout <<endl;
-	cout << "THE ANSWER IS : "<<subtract (a,b)<<endl<<endl;
+	cin >> num2;
+	cout << endl<<"THE ANSWER IS : "<<subtract (num1,num2)<<endl<<endl;
+	//this is to repeat
+	cout << "DO YOU WANNA CONTINUE WITH THE SUBTRACT? IF THAT IS RIGHT PRESS Y"<<endl;
+	cin >> answer;
+		}while (answer=="Y"|| answer =="y");
 	break;
 	
 case 3:
+	do{
 	system ("Title MULTIPLY");
-	cin >> a;
+	system ("cls");
+	cout << "PUT YOUR FIRST NUMBER : " ;
+	cin >> num1;
 	cout << "YOUR SECOND NUMBER : ";
-	cin >>b;cout <<endl;
-	cout << "THE ANSWER IS : "<<multiply (a,b)<<endl<<endl;
+	cin >>num2;cout <<endl;
+	cout << "THE ANSWER IS : "<<multiply (num1,num2)<<endl<<endl;
+	//this is to repeat
+	cout << "DO YOU WANNA CONTINUE WITH THE MULTIPLY? IF THAT IS RIGHT PRESS Y"<<endl;
+	cin >> answer;
+		}while (answer=="Y" || answer =="y");
 	break;
 	
 case 4:
-	system ("Title MULTIPLY");
-	cin >>a;
+	do{
+	system ("Title DIVISION");
+	system ("cls");
+	cout << "PUT YOUR FIRST NUMBER : " ;
+	cin >>num1;
 	cout << "YOUR SECOND NUMBER : ";
-	cin >> b;cout <<endl;
-	cout << "THE ANSWER IS : "<<division (a,b)<< endl<<endl;
+	cin >> num2;cout <<endl;
+	cout << "THE ANSWER IS : "<<division (num1,num2)<< endl<<endl;
+	//this is to repeat
+	cout << "DO YOU WANNA CONTINUE WITH THE DIVISION? IF THAT IS RIGHT PRESS Y"<<endl;
+	cin >> answer;
+		}while (answer=="Y" || answer =="y");
 	break;
 	
 	}
-	cout << "DO YOU WANT TO GO BACK TO THE START ?"<< endl << "IF YOU WANT TO USE THE CALCULATOR AGAIN  "; 
+	//this is to go back to the start
+	system ("cls");
+	cout << "DO YOU WANT TO GO BACK TO THE START ?"<< endl; 
 	cout<<"PRESS : Y"<<endl;
-	cin >> f;
+	cin >> answer;
 	}
-while (f == "y"|| f== "Y");
+while (answer == "y"|| answer== "Y");
 
 }
 
-int sum (int a, int b){
-	return a + b;
+int sum (int num1, int num2){
+	return num1 + num2;
 }
-int subtract (int a, int b){
-	return a-b;
+int subtract (int num1, int num2){
+	return num1-num2;
 }
-int multiply (int a, int b){
-	return a*b;
+int multiply (int num1, int num2){
+	return num1*num2;
 }
-int division (int a, int b){
-	return a/b;
+int division (int num1, int num2){
+	return num1/num2;
 }
